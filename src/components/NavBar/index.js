3895e549e53712652cf9a23c904a3ff94d88ff05
Caleb <caleb.n.post@gmail.com>
2017-11-19 21:20:28 -0500

    Add admin link to NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -11,6 +11,7 @@ export default class NavBar extends React.Component {
                 <ul className="nav navbar-nav navbar-right">
                     <li><Link className="navbar-item" to="/npcs">NPCs</Link></li>
                     <li><Link className="navbar-item" to="/equipment">Equipment</Link></li>
+                    <li><Link className="navbar-item" to="/admin">Admin</Link></li>
                     <li><Link className="navbar-item" to="/about">About</Link></li>
                 </ul>
             </nav>
