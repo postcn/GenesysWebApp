2bcd3f04e310cdbb9782e415e554b90d3b8c101e
Caleb <caleb.n.post@gmail.com>
2017-11-21 19:52:00 -0500

    Server has socket io running

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => {
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', require('./sockets'));
+
+server.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
---
+++ server/sockets.js
@@ -0,0 +1,5 @@
+function handleIo(client) {
+
+}
+
+module.exports = handleIo;
